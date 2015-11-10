6f63c2b26089515685b10088dd35d3d997c13e3b
Brian Retterer <brian@stormpath.com>
2015-11-09 21:19:34 -0500

    add test framework and integrations

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Stormpath\Tests;
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
