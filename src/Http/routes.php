3e9df34d01ba5aa8752de1462e3de5ecaae3d5ea
Brian Retterer <brian@stormpath.com>
2015-12-01 16:29:36 -0500

    Login setup in Routes

---
+++ src/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Stormpath Laravel Routes
+ |--------------------------------------------------------------------------
+ |
+ | These are all the routes that are possible within the application. Only
+ | routes that are enabled via the Stormpath Laravel configuration file
+ | will be loaded. Any of these routes can be overridden in by you.
+ */
+
+/*
+ |--------------------------------------------------------------------------
+ | Login Routes
+ |--------------------------------------------------------------------------
+ */
+if (config('stormpath.web.login.enabled')) {
+    $this->app->router->get( config('stormpath.web.login.uri'), ['as' => 'stormpath.login', 'uses' => 'Stormpath\Laravel\Http\Controllers\LoginController@show'] );
+    $this->app->router->post( config('stormpath.web.login.uri'), ['as' => 'stormpath.login', 'uses' => 'Stormpath\Laravel\Http\Controllers\LoginController@doLogin'] );
+}
