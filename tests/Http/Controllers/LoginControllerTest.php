6e6a94ce4ef92f4d46ac05201d0b362dc2edf008
Brian Retterer <brian@stormpath.com>
2015-12-02 14:49:56 -0500

    Login Controller tests

---
+++ tests/Http/Controllers/LoginControllerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Stormpath\Tests\Http\Controllers;
+
+use Mockery as m;
+use Stormpath\Laravel\Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    /** @test */
+    public function it_requires_a_login_to_be_submitted()
+    {
+        $this->post('login', ['password'=>'superPassword']);
+        $this->assertRedirectedTo(config('stormpath.web.login.uri'));
+        $this->followRedirects();
+        $this->seePageIs('login');
+        $this->see('Log In');
+
+        $this->assertSessionHasErrors(['login'=>'Login is required.']);
+        $this->assertHasOldInput();
+    }
+
+    /** @test */
+    public function it_requires_a_password_to_be_submitted()
+    {
+        $this->post('login', ['login' => 'someLogin']);
+        $this->assertRedirectedTo(config('stormpath.web.login.uri'));
+        $this->followRedirects();
+        $this->seePageIs('login');
+        $this->see('Log In');
+
+        $this->assertSessionHasErrors(['password' => 'Password is required.']);
+        $this->assertHasOldInput();
+
+    }
+
+    /** @test */
+    public function successful_login_sets_session_variables()
+    {
+        $account = $this->createAccount(['login' => 'test@test.com', 'password' => 'superP4ss!']);
+        $this->post('login', ['login' => 'test@test.com', 'password' => 'superP4ss!']);
+        $this->assertSessionHas(config('stormpath.web.accessTokenCookie.name'));
+        $this->assertSessionHas(config('stormpath.web.refreshTokenCookie.name'));
+        $account->delete();
+    }
+
+    /** @test */
+    public function will_display_error_if_account_is_invalid()
+    {
+        $this->post('login', ['login' => 'somelogin', 'password' => 'somePassword']);
+        $this->assertRedirectedTo(config('stormpath.web.login.uri'));
+        $this->followRedirects();
+        $this->seePageIs('login');
+        $this->see('Log In');
+
+        $this->assertSessionHasErrors(['errors'=>'Invalid username or password.']);
+        $this->assertHasOldInput();
+    }
+
+    /** @test */
+    public function will_redirect_to_next_uri_on_login()
+    {
+        $account = $this->createAccount(['login' => 'test@test.com', 'password' => 'superP4ss!']);
+        $this->post('login', ['login' => 'test@test.com', 'password' => 'superP4ss!']);
+        $this->assertRedirectedTo(config('stormpath.web.login.nextUri'));
+        $account->delete();
+
+    }
+
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Stormpath\Laravel\Tests;
+
+use Mockery as m;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+
+    private $application;
+
+    public function setup()
+    {
+        parent::setup();
+        $this->application = \Stormpath\Resource\Application::instantiate(array('name' => 'Test Application  - ' . microtime(), 'description' => 'Description of Main App', 'status' => 'enabled'));
+        self::createResource(\Stormpath\Resource\Application::PATH, $this->application, array('createDirectory' => true));
+        $href = $this->application->href;
+        $href = explode('/',$href);
+        config(['stormpath.application'=>end($href)]);
+    }
+
+    public function createAccount()
+    {
+        $account = \Stormpath\Resource\Account::instantiate([
+            'givenName' => 'Test',
+            'surname' => 'Account',
+            'email' => 'test@test.com',
+            'password' => 'superP4ss!'
+        ]);
+        $account = $this->application->createAccount($account);
+        return $account;
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return ['Stormpath\Laravel\Support\StormpathLaravelServiceProvider'];
+    }
+
+    protected static function createResource($parentHref, \Stormpath\Resource\Resource $resource, array $options = array())
+    {
+
+        if (!(strpos($parentHref, '/') === 0))
+        {
+            $parentHref = '/' . $parentHref;
+        }
+
+        $client = app('stormpath.client');
+        $resource = $client->dataStore->create($parentHref, $resource, get_class($resource), $options);
+        return $resource;
+    }
+
+    public function tearDown()
+    {
+        if ($this->application)
+        {
+            $accountStoreMappings = $this->application->accountStoreMappings;
+
+            if ($accountStoreMappings)
+            {
+                foreach($accountStoreMappings as $asm)
+                {
+                    $accountStore = $asm->accountStore;
+                    $asm->delete();
+                    $accountStore->delete();
+                }
+            }
+
+            $this->application->delete();
+            $this->application = null;
+        }
+
+        parent::tearDown();
+    }
+}
