7e6bc8231d12f188c968c981b8d3205da75dab9c
Brian Retterer <brian@stormpath.com>
2015-12-10 11:09:07 -0500

    Reset config to defaults

--- src/config/stormpath.php
+++ src/config/stormpath.php
@@ -66,7 +66,7 @@ return [
         ],
 
         "register" => [
-            "enabled" => true,
+            "enabled" => false,
             "uri" => "/register",
             "nextUri" => "/",
             "autoAuthorize" => false,
