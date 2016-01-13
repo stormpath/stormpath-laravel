26b7a606173ea527b597929fed25d90da24fe615
Dave Thomas <dave@monstergfx.com>
2016-01-13 10:56:18 -0800

    added test for firing of the UserIsLoggingIn event

---
+++ tests/Http/Controllers/LoginControllerEventTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * Copyright 2015 Stormpath, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Stormpath\Tests\Http\Controllers;
+
+use Stormpath\Laravel\Tests\TestCase;
+
+class LoginControllerEventTest extends TestCase
+{
+/*
+it fires the UserIsLoggingIn event before authentication
+it aborts the login if the UserIsLoggingIn event listener returns false
+it fires the UserHasLoggedIn event on successful login
+*/
+
+
+    /** @test */
+    public function it_fires_the_UserIsLoggingIn_event_before_authentication()
+    {
+        $this->expectsEvents(\Stormpath\Laravel\Events\UserIsLoggingIn::class);
+
+        $this->setupStormpathApplication();
+        $account = $this->createAccount(['login' => 'test@test.com', 'password' => 'superP4ss!']);
+        $this->post('login', ['login' => 'test@test.com', 'password' => 'superP4ss!']);
+
+        $this->seeCookie(config('stormpath.web.accessTokenCookie.name'));
+        $this->seeCookie(config('stormpath.web.refreshTokenCookie.name'));
+        $account->delete();
+    }
+
+}
