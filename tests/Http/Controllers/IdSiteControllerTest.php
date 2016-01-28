d007c08e0f59e5c4bd440856d27b4a26357e222c
Brian Retterer <brian@stormpath.com>
2016-01-28 17:29:54 -0500

    add Id Site Tests

---
+++ tests/Http/Controllers/IdSiteControllerTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Stormpath\Laravel\Tests\Http\Controllers;
+
+use Stormpath\Laravel\Tests\TestCase;
+
+class IdSiteControllerTest extends TestCase
+{
+    public function getEnvironmentSetUp($app)
+    {
+        parent::getEnvironmentSetUp($app);
+
+        config(['stormpath.web.idSite.enabled' => true]);
+        config(['stormpath.web.login.enabled' => true]);
+        config(['stormpath.web.logout.enabled' => true]);
+        config(['stormpath.web.register.enabled' => true]);
+        config(['stormpath.web.forgotPassword.enabled' => true]);
+    }
+
+    /** @test */
+    public function login_will_redirect_to_id_site()
+    {
+        $this->setupStormpathApplication();
+        $this->get('login');
+        $this->assertRedirectedContains('https://api.stormpath.com');
+
+    }
+
+    /** @test */
+    public function logout_will_redirect_to_id_site()
+    {
+        $this->setupStormpathApplication();
+        $this->get('logout');
+        $this->assertRedirectedContains('https://api.stormpath.com');
+
+    }
+
+    /** @test */
+    public function register_will_redirect_to_id_site()
+    {
+        $this->setupStormpathApplication();
+        $this->get('register');
+        $this->assertRedirectedContains('https://api.stormpath.com');
+
+    }
+
+    /** @test */
+    public function forgot_will_redirect_to_id_site()
+    {
+        $this->setupStormpathApplication();
+        $this->get('forgot');
+        $this->assertRedirectedContains('https://api.stormpath.com');
+
+    }
+
+
+
+
+
+
+    private function assertRedirectedContains($uri, $with = [])
+    {
+        $this->assertInstanceOf('Illuminate\Http\RedirectResponse', $this->response);
+
+        $this->assertContains($this->app['url']->to($uri), $this->response->headers->get('Location'));
+
+        $this->assertSessionHasAll($with);
+    }
+
+
+}
